e1bcd27f70348cfa12ceb97f8b0b4e8639e97edb
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-24 10:31:40 +0200

    small item suiting fixes added in CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -79,9 +79,15 @@ class CharInfo extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+
+    let imgStyle = {'objectFit': 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit': 'unset'};
+    }
+
     return (<>
         <div className="char__basics">
-            <img src={thumbnail} alt="abyss"/>
+            <img src={thumbnail} alt="abyss" style={imgStyle}/>
             <div>
                 <div className="char__info-name">{name}</div>
                 <div className="char__btns">
@@ -102,6 +108,7 @@ const View = ({char}) => {
             {comics.length > 0 ? null : "There is no comics with this character"}
             {
                 comics.map((item, i) => {
+                    if (i>=10) return;
                     return (
                         <li key={i} className="char__comics-item">
                             {item.name}
